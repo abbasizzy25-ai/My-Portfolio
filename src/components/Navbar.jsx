793476fa8406f58adba1ee8b02fa670609e8abff
abbasizzy25-ai <abbasizzy25@gmail.com>
2025-08-27 01:27:25 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { div } from 'framer-motion/client';
+import React, { useEffect, useState} from 'react';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
